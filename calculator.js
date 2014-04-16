98144eb8aa737ecbdc79ff440a5c217ed7d9c3ec
jcyoder <kajcyoder@comcast.net>
2014-04-15 20:02:36 -0600

    moved javascript into its own .js file.   worked on onmouseup/down functionality

--- calculator.js
+++ calculator.js
@@ -1,4 +1,36 @@
+// use strict
 
+ 
+function numPressed(num) {
+  //  var alertMessage = "You clicked the " + num + " button";
+  //  alert(alertMessage);
+    updateDisplay(num);
+}
+
+function updateDisplay(num) {
+     if( document.getElementById("display").value == "0.") {
+        document.getElementById("display").value = num;
+     } else {
+         document.getElementById("display").value += num;
+     }
+
+}
+
+function clearNumbers() {
+
+    document.getElementById("display").value = "0.";
+}
+
+function addOperator(num) {
+ document.getElementById("display").value += num;
+}
+           
+ function mouseDown() {
+  
+     this.style.backgroundColor="#333333";
+     this.style.border="3px solid blue";
+ }
+    
 
 
     
